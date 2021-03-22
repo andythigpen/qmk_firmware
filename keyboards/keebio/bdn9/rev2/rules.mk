b6d60f445d31e47377043fe80e1d3ac6f4d5a59f
Andrew Thigpen <andyt05@gmail.com>
2021-03-21 20:53:38 -0500

    Added custom keymap.

--- keyboards/keebio/bdn9/rev2/rules.mk
+++ keyboards/keebio/bdn9/rev2/rules.mk
@@ -24,3 +24,6 @@ RGB_MATRIX_DRIVER = WS2812
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
+
+VIRTSER_ENABLE = yes
+# EXTRAFLAGS += -DDEBUG_ACTION
