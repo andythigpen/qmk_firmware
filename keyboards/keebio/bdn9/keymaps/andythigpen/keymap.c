f0b94494680185192eb19ec7ca53541cbf550563
Andrew Thigpen <andyt05@gmail.com>
2021-04-04 10:21:50 -0500

    Update keymaps, add indicators per-layer.

--- keyboards/keebio/bdn9/keymaps/andythigpen/keymap.c
+++ keyboards/keebio/bdn9/keymaps/andythigpen/keymap.c
@@ -45,13 +45,23 @@ enum custom_keycodes {
     TEST_SUITE,
     TEST_FILE,
     TEST_NEAREST,
-    RUN_SERVICE,
-    MUTE_TEAMS,
-    MUTE_SLACK,
+    RUN_SERVICE,        // Start!<CR>
+    MUTE_TEAMS,         // cmd+shift+m
+    MUTE_TEAMS_FOCUS,   // cmd+shift+F19
+    FOCUS_TEAMS,        // shift+F19
+    MUTE_SLACK,         // m
+    MUTE_SLACK_FOCUS,   // cmd+shift+F18
+    FOCUS_SLACK,        // shift+F18
     MUTE_STATUS,
     START_SLACK,
     START_TEAMS,
-    END_CALL
+    END_CALL,
+    CUSTOM_MACRO1,      // cmd+F13
+    CUSTOM_MACRO2,      // cmd+F14
+    CUSTOM_MACRO3,      // cmd+F15
+    CUSTOM_MACRO4,      // cmd+F16
+    CUSTOM_MACRO5,      // cmd+F17
+    CUSTOM_MACRO6,      // cmd+F18
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -63,12 +73,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_DEFAULT_LAYER] = LAYOUT(
         KC_MUTE,  TO(_PROGRAMMING_LAYER),  START_SLACK,
-        KC_F13,   KC_F14,  KC_F15,
-        KC_F16,   KC_F17,  KC_F18
+        CUSTOM_MACRO1, CUSTOM_MACRO2, CUSTOM_MACRO3,
+        CUSTOM_MACRO4, CUSTOM_MACRO5, CUSTOM_MACRO6
     ),
     /* Programming */
     /*
-        | _          | Layer 0   | Debugging Layer |
+        | _          | Layer 0   | Slack Layer |
         | Test Last  | Start dbg | Run Service  |
         | Test Suite | Test File | Test Nearest |
      */
@@ -91,22 +101,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
         | _         | Layer 0   | Teams layer |
         | Change mute status | Mute call | Mute call |
-        | Mute call | Mute call | Mute call |
+        | Focus app | Mute call | Mute call |
      */
     [_SLACK_LAYER] = LAYOUT(
         _______,       END_CALL,        START_TEAMS,
         MUTE_STATUS,    MUTE_SLACK,      MUTE_SLACK,
-        MUTE_SLACK,    MUTE_SLACK,      MUTE_SLACK
+        FOCUS_SLACK,    MUTE_SLACK_FOCUS,      MUTE_SLACK_FOCUS
     ),
     /*
         | _         | Layer 0   | Teams layer |
         | Change mute status | Mute call | Mute call |
-        | Mute call | Mute call | Mute call |
+        | Focus app | Mute call | Mute call |
      */
     [_TEAMS_LAYER] = LAYOUT(
         _______,       END_CALL,        START_SLACK,
         MUTE_STATUS,    MUTE_TEAMS,      MUTE_TEAMS,
-        MUTE_TEAMS,    MUTE_TEAMS,      MUTE_TEAMS
+        FOCUS_TEAMS,    MUTE_TEAMS_FOCUS,      MUTE_TEAMS_FOCUS
     ),
 };
 
@@ -118,7 +128,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
     case END_DEBUG:
         if (record->event.pressed) {
-            SEND_STRING(":VimspectorReset" SS_TAP(X_ENTER));
+            SEND_STRING(SS_TAP(X_ESCAPE) ":VimspectorReset" SS_TAP(X_ENTER));
             layer_move(_PROGRAMMING_LAYER);
         }
         break;
@@ -130,19 +140,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         break;
     case TEST_LAST:
         if (record->event.pressed)
-            SEND_STRING(":TestLast" SS_TAP(X_ENTER));
+            SEND_STRING(SS_TAP(X_ESCAPE) ":TestLast" SS_TAP(X_ENTER));
         break;
     case TEST_SUITE:
         if (record->event.pressed)
-            SEND_STRING(":TestSuite" SS_TAP(X_ENTER));
+            SEND_STRING(SS_TAP(X_ESCAPE) ":TestSuite" SS_TAP(X_ENTER));
         break;
     case TEST_FILE:
         if (record->event.pressed)
-            SEND_STRING(":TestFile" SS_TAP(X_ENTER));
+            SEND_STRING(SS_TAP(X_ESCAPE) ":TestFile" SS_TAP(X_ENTER));
         break;
     case TEST_NEAREST:
         if (record->event.pressed)
-            SEND_STRING(":TestNearest" SS_TAP(X_ENTER));
+            SEND_STRING(SS_TAP(X_ESCAPE) ":TestNearest" SS_TAP(X_ENTER));
+        break;
+    case RUN_SERVICE:
+        if (record->event.pressed)
+            SEND_STRING(SS_TAP(X_ESCAPE) ":Start!" SS_TAP(X_ENTER));
         break;
     case MUTE_TEAMS:
         if (record->event.pressed) {
@@ -150,11 +164,31 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             set_mute_status(!muted_status);
         }
         break;
+    case MUTE_TEAMS_FOCUS:
+        if (record->event.pressed) {
+            SEND_STRING(SS_LCMD(SS_LSFT(SS_TAP(X_F19))));
+            set_mute_status(!muted_status);
+        }
+        break;
+    case FOCUS_TEAMS:
+        if (record->event.pressed)
+            SEND_STRING(SS_LSFT(SS_TAP(X_F19)));
+        break;
     case MUTE_SLACK:
         if (record->event.pressed) {
             SEND_STRING("m");
             set_mute_status(!muted_status);
         }
+        break;
+    case MUTE_SLACK_FOCUS:
+        if (record->event.pressed) {
+            SEND_STRING(SS_LCMD(SS_LSFT(SS_TAP(X_F18))));
+            set_mute_status(!muted_status);
+        }
+        break;
+    case FOCUS_SLACK:
+        if (record->event.pressed)
+            SEND_STRING(SS_LSFT(SS_TAP(X_F18)));
         break;
     case START_SLACK:
         if (record->event.pressed) {
@@ -178,12 +212,36 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         if (record->event.pressed)
             set_mute_status(!muted_status);
         break;
+    case CUSTOM_MACRO1:
+        if (record->event.pressed)
+            SEND_STRING(SS_LCMD(SS_TAP(X_F13)));
+        break;
+    case CUSTOM_MACRO2:
+        if (record->event.pressed)
+            SEND_STRING(SS_LCMD(SS_TAP(X_F14)));
+        break;
+    case CUSTOM_MACRO3:
+        if (record->event.pressed)
+            SEND_STRING(SS_LCMD(SS_TAP(X_F15)));
+        break;
+    case CUSTOM_MACRO4:
+        if (record->event.pressed)
+            SEND_STRING(SS_LCMD(SS_TAP(X_F16)));
+        break;
+    case CUSTOM_MACRO5:
+        if (record->event.pressed)
+            SEND_STRING(SS_LCMD(SS_TAP(X_F17)));
+        break;
+    case CUSTOM_MACRO6:
+        if (record->event.pressed)
+            SEND_STRING(SS_LCMD(SS_TAP(X_F18)));
+        break;
     }
     return true;
 };
 
 void keyboard_post_init_user(void) {
-  debug_enable = true;
+  /* debug_enable = true; */
   // debug_matrix = true;
   // debug_keyboard=true;
   // debug_mouse=true;
@@ -234,7 +292,7 @@ void encoder_update_user(uint8_t index, bool clockwise) {
 #define STATE_LEN               1
 #define STATE_BUF               2
 
-#define MAX_BUF_LEN             4
+#define MAX_BUF_LEN             5
 
 #define CMD_RESET               1   // reset the keyboard to flash
 #define CMD_SET_MODE            2   // set mode
@@ -252,8 +310,11 @@ void encoder_update_user(uint8_t index, bool clockwise) {
 uint8_t cmd, len, buf_idx;
 uint8_t buf[MAX_BUF_LEN];
 uint8_t state = STATE_CMD;
-HSV indicator_hsv[RGBLED_NUM];
-bool indicator_enable[RGBLED_NUM];
+
+// key indicators
+#define INDICATOR_LAYERS 5
+HSV indicator_hsv[INDICATOR_LAYERS][RGBLED_NUM];
+bool indicator_enable[INDICATOR_LAYERS][RGBLED_NUM];
 
 void process_cmd(void) {
     switch (cmd) {
@@ -285,37 +346,38 @@ void process_cmd(void) {
             break;
 
         case CMD_TOGGLE_INDICATOR:
-            // { index }
-            dprintf("toggle led:%d\n", buf[0]);
-            if (buf[0] < RGBLED_NUM) {
-                indicator_enable[buf[0]] = !indicator_enable[buf[0]];
+            // { layer, index }
+            dprintf("toggle layer:%d led:%d\n", buf[0], buf[1]);
+            if (buf[0] < INDICATOR_LAYERS && buf[1] < RGBLED_NUM) {
+                indicator_enable[buf[0]][buf[1]] = !indicator_enable[buf[0]][buf[1]];
             }
             break;
 
         case CMD_ENABLE_INDICATOR:
-            // { index }
-            dprintf("enable led:%d\n", buf[0]);
-            if (buf[0] < RGBLED_NUM) {
-                indicator_enable[buf[0]] = true;
+            // { layer, index }
+            dprintf("enable layer:%d led:%d\n", buf[0], buf[1]);
+            if (buf[0] < INDICATOR_LAYERS && buf[1] < RGBLED_NUM) {
+                indicator_enable[buf[0]][buf[1]] = true;
             }
             break;
 
         case CMD_DISABLE_INDICATOR:
-            // { index }
-            dprintf("disable led:%d\n", buf[0]);
-            if (buf[0] < RGBLED_NUM) {
-                indicator_enable[buf[0]] = false;
+            // { layer, index }
+            dprintf("disable layer:%d led:%d\n", buf[0], buf[1]);
+            if (buf[0] < INDICATOR_LAYERS && buf[1] < RGBLED_NUM) {
+                indicator_enable[buf[0]][buf[1]] = false;
             }
             break;
 
         case CMD_SET_INDICATOR_HSV: {
-            // { index, h, s, v }
-            dprintf("set indicator led:%d h:%d s:%d v:%d\n", buf[0], buf[1], buf[2], buf[3]);
-            if (buf[0] < RGBLED_NUM) {
-                HSV *hsv = &indicator_hsv[buf[0]];
-                hsv->h = buf[1];
-                hsv->s = buf[2];
-                hsv->v = buf[3];
+            // { layer, index, h, s, v }
+            dprintf("set indicator layer:%d led:%d h:%d s:%d v:%d\n",
+                    buf[0], buf[1], buf[2], buf[3], buf[4]);
+            if (buf[0] < INDICATOR_LAYERS && buf[1] < RGBLED_NUM) {
+                HSV *hsv = &indicator_hsv[buf[0]][buf[1]];
+                hsv->h = buf[2];
+                hsv->s = buf[3];
+                hsv->v = buf[4];
             }
             break;
         }
@@ -394,22 +456,26 @@ void virtser_recv(uint8_t serIn) {
 }
 
 void rgb_matrix_indicators_kb(void) {
+    uint32_t layer = get_highest_layer(layer_state);
+    if (layer >= INDICATOR_LAYERS)
+        return;
+
     for (uint8_t i = 0; i < RGBLED_NUM; ++i) {
-        if (!indicator_enable[i])
+        if (!indicator_enable[layer][i])
             continue;
 
-        HSV *hsv = &indicator_hsv[i];
+        HSV *hsv = &indicator_hsv[layer][i];
         RGB rgb = hsv_to_rgb(*hsv);
         rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
     }
 }
 
-void set_led_under_hsv(uint8_t h, uint8_t s, uint8_t v) {
-    indicator_hsv[LED_UNDER_L].h = h;
-    indicator_hsv[LED_UNDER_L].s = s;
-    indicator_hsv[LED_UNDER_L].v = v;
-    indicator_hsv[LED_UNDER_R] = indicator_hsv[LED_UNDER_L];
-    indicator_enable[LED_UNDER_L] = indicator_enable[LED_UNDER_R] = true;
+void set_led_under_hsv(uint32_t layer, uint8_t h, uint8_t s, uint8_t v) {
+    indicator_hsv[layer][LED_UNDER_L].h = h;
+    indicator_hsv[layer][LED_UNDER_L].s = s;
+    indicator_hsv[layer][LED_UNDER_L].v = v;
+    indicator_hsv[layer][LED_UNDER_R] = indicator_hsv[layer][LED_UNDER_L];
+    indicator_enable[layer][LED_UNDER_L] = indicator_enable[layer][LED_UNDER_R] = true;
 }
 
 void set_mute_status(bool muted) {
@@ -433,21 +499,22 @@ void end_call(void) {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     dprintf("layer_state %x\n", state);
-    switch (get_highest_layer(state)) {
+    uint32_t layer = get_highest_layer(state);
+    switch (layer) {
         case _DEFAULT_LAYER:
-            set_led_under_hsv(0, 0, 200);
+            set_led_under_hsv(layer, 0, 0, 100);
             break;
         case _PROGRAMMING_LAYER:
-            set_led_under_hsv(150, 250, 255);
+            set_led_under_hsv(layer, 150, 250, 255);
             break;
         case _DEBUGGING_LAYER:
-            set_led_under_hsv(10, 250, 255);
+            set_led_under_hsv(layer, 10, 250, 255);
             break;
         case _SLACK_LAYER:
-            set_led_under_hsv(30, 250, 255);
+            set_led_under_hsv(layer, 30, 250, 255);
             break;
         case _TEAMS_LAYER:
-            set_led_under_hsv(180, 250, 255);
+            set_led_under_hsv(layer, 180, 250, 255);
             break;
     }
     return state;
